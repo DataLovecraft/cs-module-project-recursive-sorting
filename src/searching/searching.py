91653d8c37f697572827a52fbc629eb264025e9d
John Mercedes <mercedes.john@gmail.com>
2020-07-23 16:50:25 -0400

    updates to module

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,37 +1,25 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target):
-    # Your code here
-    # we're searching in between the low and high indices 
-    low = 0
-    high = len(arr) - 1
-​
-    # loop so long as low hasn't moved passed high
-    while low <= high:
-        mid = (low + high) // 2
-​
-        # base case where we've found our target 
-        if arr[mid] == target:
-            return mid
-        elif target < arr[mid]:
-            # toss out the right side since the target 
-            # has to be on the left side 
-            # do this by setting high to be mid - 1
-            high = mid - 1
-        else:
-            # toss out the left side since the target
-            # has to be on the right side 
-            # do this by setting low to mid + 1
-            low = mid + 1
-​
-    return -1  # not found
+def binary_search(arr, target, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+    if target == arr[mid]:
+        return mid
+
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid - 1)
+    else:
+        return binary_search(arr, target, mid+1, end)
 
 
-# STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
-# the target regardless of whether the input array is
-# sorted in ascending order or in descending order
-# You can implement this function either recursively 
-# or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    if arr[0] > arr[-1]:
+        is_ascending = False
+    else:
+        is_ascending = True
 
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    else:
+        return binary_search(arr, target, len(arr) - 1)
